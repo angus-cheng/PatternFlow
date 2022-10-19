3dc3c46fe926025473b2a4fcc6d506f1825658dd
Angus Cheng <anguscheng389@gmail.com>
2022-10-19 21:54:35 +1000

    test: Tested loading image into numpy array to look at image with mask applied.

--- recognition/45330623-ISIC-UNET/dataset.py
+++ recognition/45330623-ISIC-UNET/dataset.py
@@ -6,6 +6,7 @@ import zipfile
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 def dir_path_exist(path):
     path = path.replace('\\', '/')
@@ -55,9 +56,30 @@ training_ground_truth_url = 'https://isic-challenge-data.s3.amazonaws.com/2017/I
 download_data(training_data_url, path)
 download_data(training_ground_truth_url, path)
 
-train_dir = './data/ISIC-2017_Training_Data'
+image_dir = './data/ISIC-2017_Training_Data'
 mask_dir = './data/ISIC-2017_Training_Part1_GroundTruth'
 annotations_file = './data/ISIC-2017_Training_Data/ISIC-2017_Training_Data_metadata.csv'
 
-df = create_df(train_dir)
+df = create_df(image_dir)
 print(len(df))
+
+X_trainval, X_test = train_test_split(df['id'].values, test_size=0.1, random_state=19)
+X_train, X_val = train_test_split(X_trainval, test_size=0.15, random_state=19)
+
+print('Train Size   : ', len(X_train))
+print('Val Size     : ', len(X_val))
+print('Test Size    : ', len(X_test))
+
+img = torchvision.io.read_image(image_dir + '/' + df['id'][100] + '.jpg')
+img = torchvision.transforms.Resize((512, 512))(img)
+img = torch.transpose(img, 2, 0)
+mask = torchvision.io.read_image(mask_dir + '/' + df['id'][100] + '_segmentation.png')
+mask = torchvision.transforms.Resize((512, 512))(mask)
+mask = torch.transpose(mask, 2, 0) 
+print('Image Size', np.asarray(img).shape)
+print('Mask Size', np.asarray(mask).shape)
+
+plt.imshow(img)
+plt.imshow(mask, alpha=0.6)
+plt.title('Picture with Mask Appplied')
+plt.show()
