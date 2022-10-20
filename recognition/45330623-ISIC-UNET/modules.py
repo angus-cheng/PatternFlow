6bad35d754f14973d49850d4c6e2953c14fa2686
Angus Cheng <anguscheng389@gmail.com>
2022-10-21 05:27:48 +1000

    fix: Updated ImprovedUnetModel class
    Fixed layer input and output size errors

--- recognition/45330623-ISIC-UNET/modules.py
+++ recognition/45330623-ISIC-UNET/modules.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torchsummary import summary
 
 def dice_coef(y_true, y_pred, smooth):
     y_true_f = torch.flatten(y_true)
@@ -13,21 +14,22 @@ def dice_coef_loss(y_true, y_pred):
 class ContextLayer(nn.Module):
     def __init__(self, input, output_filter):
         super(ContextLayer, self).__init__()
+        self.input = input
+        self.output_filter = output_filter
         self.padding = 'same'
-        self.initial_output = 16
         self.context_dropout = 0.3
         self.leaky_alpha = 1e-2
         self.kernel_size = (3, 3)
 
         self.leaky_relu = nn.LeakyReLU(self.leaky_alpha)
         self.context_module = nn.Sequential(
-            nn.InstanceNorm2d(input),
+            nn.InstanceNorm2d(self.input),
             self.leaky_relu,
-            nn.Conv2d(in_channels=input, out_channels=output_filter, kernel_size=self.kernel_size, padding=self.padding),
+            nn.Conv2d(in_channels=self.input, out_channels=output_filter, kernel_size=self.kernel_size, padding=self.padding),
             nn.Dropout(self.context_dropout),
-            nn.InstanceNorm2d(input),
+            nn.InstanceNorm2d(self.input),
             self.leaky_relu,
-            nn.Conv2d(in_channels=input, out_channels=output_filter, kernel_size=self.kernel_size, padding=self.padding),
+            nn.Conv2d(in_channels=self.input, out_channels=output_filter, kernel_size=self.kernel_size, padding=self.padding),
         )
 
     def forward(self, x):
@@ -47,147 +49,156 @@ class ContextLayer(nn.Module):
 #         return x
 
 class LocalisationLayer(nn.Module):
-    def __init__(self, input, output_filters):
+    def __init__(self, input, output_filter):
         super(LocalisationLayer, self).__init__()
         self.input = input
-        self.output_filters = output_filters
+        self.output_filter = output_filter
         self.padding = 'same'
         self.leaky_alpha = 1e-2
 
-        self.conv1 = nn.Conv2d(input, self.output_filters, kernel_size=(3, 3), padding=self.padding)
+        self.conv1 = nn.Conv2d(input, self.output_filter, kernel_size=(3, 3), padding=self.padding)
         self.leaky_relu = nn.LeakyReLU(self.leaky_alpha)
-        self.normalize = nn.LazyInstanceNorm2d()
-        self.conv2 = nn.Conv2d(input, self.output_filters, kernel_size=(1, 1), padding=self.padding)
+        # self.normalize = nn.InstanceNorm2d(self.output_filter)
+        self.conv2 = nn.Conv2d(self.output_filter, self.output_filter, kernel_size=(1, 1), padding=self.padding)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.leaky_relu(x)
-        x = self.normalize(x)
+        # x = self.normalize(x)
         x = self.conv2(x)
         x = self.leaky_relu(x)
-        x = self.normalize(x)
+        # x = self.normalize(x)
         return x
 
 
 class ImprovedUnetModel(nn.Module):
     def __init__(self):
         super(ImprovedUnetModel, self).__init__()
-        self.padding = 'same'
         self.initial_output = 16
-        self.context_dropout = 0.3
         self.leaky_alpha = 1e-2
 
         # Encoder: Level one
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.initial_output, kernel_size=(3, 3), padding=self.padding)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=(3, 3))
         self.leaky_relu = nn.LeakyReLU(self.leaky_alpha)
-        self.context_module = ContextLayer(self.conv1.out_channels, self.initial_output)
+        self.context_module = ContextLayer(16, 16)
 
         # Encoder: Level two
-        self.conv2 = nn.LazyConv2d(self.initial_output * 2, (3, 3), stride=(2, 2))
-        self.context_module2 = ContextLayer(self.conv2.out_channels, self.initial_output * 2)
+        self.conv2 = nn.Conv2d(16, 32, (3, 3), stride=(2, 2))
+        self.context_module2 = ContextLayer(32, 32)
 
         # Encoder: Level three
-        self.conv3 = nn.LazyConv2d(self.initial_output * 4, (3, 3), stride=(2, 2))
-        self.context_module3 = ContextLayer(self.conv3.out_channels, self.initial_output * 4)
+        self.conv3 = nn.Conv2d(32, 64, (3, 3), stride=(2, 2))
+        self.context_module3 = ContextLayer(64, 64)
 
         # Encoder: Level four
-        self.conv4 = nn.LazyConv2d(self.initial_output * 8, (3, 3), stride=(2, 2))
-        self.context_module4 = ContextLayer(self.conv4.out_channels, self.initial_output * 8)
+        self.conv4 = nn.Conv2d(64, 128, (3, 3), stride=(2, 2))
+        self.context_module4 = ContextLayer(128, 128)
 
         # Encoder: Level five 
-        self.conv5 = nn.LazyConv2d(self.initial_output * 16, (3, 3), stride=(2, 2))
-        self.context_module5 = ContextLayer(self.conv5.out_channels, self.initial_output * 16 )
+        self.conv5 = nn.Conv2d(128, 256, (3, 3), stride=(2, 2))
+        self.context_module5 = ContextLayer(256, 256)
 
         # Upsampling layer
-        self.upsample = nn.Upsample((2, 2), mode='bilinear')
-        self.conv6 = nn.LazyConv2d(self.initial_output * 16, (3, 3))
-        self.normalize = nn.LazyInstanceNorm2d()
+        self.upsample5 = nn.Upsample((63, 63), mode='bilinear')
+        self.conv6 = nn.Conv2d(256, 128, (2, 2))
+        # self.normalize = nn.InstanceNorm2d(128)
 
         # Decoder: Level four
-        self.localise4 = LocalisationLayer(self.conv6.out_channels, self.initial_output * 8)
-        self.upsample = nn.Upsample((2, 2), mode='bilinear')
-        self.conv7 = nn.LazyConv2d(self.initial_output * 8, (3, 3))
-        self.normalize = nn.LazyInstanceNorm2d()
+        self.localise4 = LocalisationLayer(256, 128)
+        self.upsample4 = nn.Upsample((127, 127), mode='bilinear')
+        self.conv7 = nn.Conv2d(128, 128, (2, 2))
+        # self.normalize = nn.InstanceNorm2d(self.initial_output * 4)
 
         # Decoder: Level three
-        self.localise3 = LocalisationLayer(self.conv7.out_channels, self.initial_output * 4)
+        self.localise3 = LocalisationLayer(192, 64)
 
         # Segment 1
-        self.conv8 = nn.LazyConv2d(1, (1, 1))
+        self.conv8 = nn.Conv2d(64, 1, (1, 1))
         
         # Upsample
-        self.conv9 = nn.LazyConv2d(self.initial_output * 2, (3, 3))
+        self.upsample3 = nn.Upsample((256, 256), mode='bilinear')
+        self.conv9 = nn.Conv2d(64, 16, (3, 3))
 
         # Decoder: Level two 
-        self.localise2 = LocalisationLayer(self.conv9.out_channels, self.initial_output * 2)
+        self.localise2 = LocalisationLayer(48, 16)
 
         # Segment 2
-        self.conv10 = nn.LazyConv2d(1, (1, 1))
+        self.conv10 = nn.Conv2d(16, 1, (1, 1))
 
         # Upsample
-        self.conv11 = nn.LazyConv2d(self.initial_output, (3, 3))
+        self.upsample2 = nn.Upsample((512, 512), mode='bilinear')
+        self.conv11 = nn.Conv2d(16, self.initial_output, (3, 3))
+
+        # Upsample Skip-Add 1
+        self.upsample_skip_add1 = nn.Upsample((254, 254), mode='bilinear')
 
         # Decoder: Level one
-        self.conv12 = nn.LazyConv2d(self.initial_output * 2, (3, 3))
+        self.conv12 = nn.Conv2d(32, self.initial_output * 2, (3, 3))
 
         # Segment 3
-        self.conv13 = nn.LazyConv2d(1, (1, 1))
+        self.conv13 = nn.Conv2d(32, 1, (1, 1))
+
+        # Upsample Skip-Add 2
+        self.upsample_skip_add2 = nn.Upsample((508, 508), mode='bilinear')
 
         # Activation
         self.output = nn.Sigmoid()
 
     def forward(self, x):
         # Encoder: Level one
+        x = x.float()
         x = self.conv1(x)
         x = self.leaky_relu(x)
-        context = self.context_module(x, self.initial_output)
+        context = self.context_module(x)
         first_level = torch.add(x, context)
         first_skip = first_level
 
         # Encoder: Level two
         x = self.conv2(first_level)
         x = self.leaky_relu(x)
-        context2 = self.context_module2(x, self.initial_output * 2)
+        context2 = self.context_module2(x)
         second_level = torch.add(x, context2)
         second_skip = second_level
 
         # Encoder: Level three
         x = self.conv3(second_level)
         x = self.leaky_relu(x)
-        context3 = self.context_module3(x, self.initial_output * 4)
+        context3 = self.context_module3(x)
         third_level = torch.add(x, context3)
         third_skip = third_level
 
         # Encoder: Level four
         x = self.conv4(third_level)
         x = self.leaky_relu(x)
-        context4 = self.context_module4(x, self.initial_output * 8)
+        context4 = self.context_module4(x)
         fourth_level = torch.add(x, context4)
         fourth_skip = fourth_level
 
         # Encoder: Level five
-        x = self.conv4(fourth_level)
+        x = self.conv5(fourth_level)
         x = self.leaky_relu(x)
-        context5 = self.context_module5(x, self.initial_output * 16)
+        context5 = self.context_module5(x)
         fifth_level = torch.add(x, context5)
         
         # Upsampling layer
-        x = self.upsample(fifth_level)
+        x = self.upsample5(fifth_level)
         x = self.conv6(x)
         x = self.leaky_relu(x)
-        x = self.normalize(x)
+        # x = self.normalize(x)
 
-        x = torch.cat((x, fourth_skip), 1)
+        x = torch.cat([x, fourth_skip], dim=1)
 
         # Decoder: Level four
         x = self.localise4(x)
-        x = self.upsample(x)
+        x = self.upsample4(x)
         x = self.conv7(x)
         x = self.leaky_relu(x)
-        x = self.normalize(x)
+        # x = self.normalize(x)
 
         # Decoder: Level three
-        x = torch.cat((x, third_skip), 1)
+        print(x.shape)
+        print(third_skip.shape)
+        x = torch.cat((x, third_skip), dim=1)
         x = self.localise3(x)
         segment1 = x
 
@@ -196,13 +207,13 @@ class ImprovedUnetModel(nn.Module):
         segment1 = self.leaky_relu(segment1)
 
         # Upsampling layer
-        x = self.upsample(x)
+        x = self.upsample3(x)
         x = self.conv9(x)
         x = self.leaky_relu(x)
-        x = self.normalize(x)
+        # x = self.normalize(x)
 
         # Decoder: Level two
-        x = torch.cat((x, second_skip), 1)
+        x = torch.cat((x, second_skip), dim=1)
         x = self.localise2(x)
         segment2 = x
 
@@ -211,25 +222,27 @@ class ImprovedUnetModel(nn.Module):
         segment2 = self.leaky_relu(segment2)
 
         # Upsampling layer
-        x = self.upsample(x)
+        x = self.upsample2(x)
         x = self.conv11(x)
         x = self.leaky_relu(x)
-        x = self.normalize(x)
+        # x = self.normalize(x)
 
         # Skip-Add 1
-        up_scaled_segment1 = self.upsample(segment1)
+        up_scaled_segment1 = self.upsample_skip_add1(segment1)
         skip_sum1 = torch.add(up_scaled_segment1, segment2)
 
         # Decoder: Level one
-        x = torch.cat((x, first_skip), 1)
+        x = torch.cat((x, first_skip), dim=1)
         x = self.conv12(x)
         x = self.leaky_relu(x)
+        segment3 = x
 
         # Segmentation 3
-        segment3 = self.conv13(x)
+        segment3 = self.conv13(segment3)
+        segment3 = self.leaky_relu(segment3)
 
         # Skip-Add 2
-        up_scaled_segment2 = self.upsample(skip_sum2)
+        up_scaled_segment2 = self.upsample_skip_add2(segment2)
         skip_sum2 = torch.add(up_scaled_segment2, segment3)
 
         x = self.output(skip_sum2)
@@ -238,4 +251,5 @@ class ImprovedUnetModel(nn.Module):
 
 device = "cuda"
 model = ImprovedUnetModel().to(device)
-print(model)
+# print(model)
+summary(model, input_size=(3, 512, 512))
