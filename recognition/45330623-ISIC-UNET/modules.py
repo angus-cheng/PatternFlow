bf2ab4d56d8efa7b6f5a828c57fa24c054cc36b6
Angus Cheng <anguscheng389@gmail.com>
2022-10-23 14:53:54 +1000

    fix: Fixed ImprovedUnetModel
    ImprovedUNetModel now correctly outputs the target shape so it can be
    analysed through training and validation.

--- recognition/45330623-ISIC-UNET/modules.py
+++ recognition/45330623-ISIC-UNET/modules.py
@@ -11,18 +11,18 @@ def dice_coef(y_true, y_pred, smooth):
 def dice_coef_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
 
-class double_conv(nn.Module):
+class ContextModule(nn.Module):
     """(conv => BN => ReLU) * 2"""
 
     def __init__(self, in_ch, out_ch):
-        super(double_conv, self).__init__()
+        super(ContextModule, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True),
+            nn.LeakyReLU(inplace=False),
             nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True),
+            nn.LeakyReLU(inplace=False),
         )
 
     def forward(self, x):
@@ -33,7 +33,7 @@ class double_conv(nn.Module):
 class inconv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(inconv, self).__init__()
-        self.conv = double_conv(in_ch, out_ch)
+        self.conv = ContextModule(in_ch, out_ch)
 
     def forward(self, x):
         x = self.conv(x)
@@ -43,23 +43,58 @@ class inconv(nn.Module):
 class down(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(down, self).__init__()
-        self.mpconv = nn.Sequential(nn.MaxPool2d(2), double_conv(in_ch, out_ch))
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2), 
+            ContextModule(in_ch, out_ch),
+            nn.LeakyReLU(inplace=False))
 
+        self.context = nn.Sequential(
+            nn.MaxPool2d(2),
+            ContextModule(in_ch, out_ch)
+        )
     def forward(self, x):
-        x = self.mpconv(x)
+        x1 = self.mpconv(x)
+        x2 = self.context(x)
+        x = torch.add(x1, x2)
         return x
 
+class LocalisationModule(nn.Module):
+    """(conv => BN => ReLU) * 2"""
+
+    def __init__(self, in_ch, out_ch):
+        super(LocalisationModule, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.LeakyReLU(inplace=False),
+            nn.BatchNorm2d(out_ch),
+            # nn.Conv2d(out_ch, out_ch, (1, 1), padding=1),
+            nn.LeakyReLU(inplace=False),
+            nn.BatchNorm2d(out_ch),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class SegmentationLayer(nn.Module):
+
+    def __init__(self, in_ch):
+        super(SegmentationLayer, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, 1, 1, padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
 class up(nn.Module):
-    def __init__(self, in_ch, out_ch, bilinear=True):
+    def __init__(self, in_ch, out_ch):
         super(up, self).__init__()
+        self.up = nn.Upsample((out_ch, out_ch), mode="bilinear", align_corners=True)
 
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(in_ch // 2, in_ch // 2, 2, stride=2)
-
-        self.conv = double_conv(in_ch, out_ch)
+        self.conv = LocalisationModule(in_ch, out_ch)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -84,18 +119,24 @@ class outconv(nn.Module):
         return x
 
 
-class UNet(nn.Module):
+class ImprovedUNetModel(nn.Module):
     def __init__(self, n_channels, n_classes):
-        super(UNet, self).__init__()
+        super(ImprovedUNetModel, self).__init__()
         self.inc = inconv(n_channels, 64)
         self.down1 = down(64, 128)
         self.down2 = down(128, 256)
         self.down3 = down(256, 512)
         self.down4 = down(512, 512)
-        self.up1 = up(1024, 256, False)
-        self.up2 = up(512, 128, False)
-        self.up3 = up(256, 64, False)
-        self.up4 = up(128, 64, False)
+        self.up1 = up(1024, 256)
+        self.up2 = up(512, 128)
+        self.seg1 = SegmentationLayer(128)
+        self.upsample_seg1 = nn.Upsample((256, 256), mode='bilinear', align_corners=True)
+        self.up3 = up(256, 64)
+        self.seg2 = SegmentationLayer(64)
+        self.upsample_seg2 = nn.Upsample((256, 256), mode='bilinear', align_corners=True)
+        self.up4 = up(128, 64)
+        self.seg3 = SegmentationLayer(64)
+        self.upsample_seg3 = nn.Upsample((256, 256), mode='bilinear', align_corners=True)
         self.outc = outconv(64, n_classes)
 
     def forward(self, x):
@@ -107,12 +148,19 @@ class UNet(nn.Module):
         x5 = self.down4(x4)
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
+        seg1 = self.seg1(x)
+        seg1 = self.upsample_seg1(seg1)
         x = self.up3(x, x2)
+        seg2 = self.seg2(x)
+        seg2 = self.upsample_seg2(seg2)
+        seg2 = torch.add(seg1, seg2)
         x = self.up4(x, x1)
+        seg3 = self.seg3(x)
+        seg3 = self.upsample_seg3(seg3)
+        seg3 = torch.add(seg2, seg3)
         x = self.outc(x)
         return torch.sigmoid(x)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = UNet(3, 4).to(device)
-# print(model)
+model = ImprovedUNetModel(3, 4).to(device)
 summary(model, input_size=(3, 512, 512))
